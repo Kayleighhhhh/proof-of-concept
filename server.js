e1838a8cc842c282e1ca4fe94794092b38d320c4
Kayleighhhhh <kayleigh.vdham04@gmail.com>
2026-06-03 11:24:43 +0200

    base opzet

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 console.log('Hier komt je server voor Sprint 12.')
+
+import express from 'express'
+
+import { Liquid } from 'liquidjs'
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static('public'))
+
+const engine = new Liquid()
+app.engine('liquid', engine.express())
+
+app.set('views', './views')
+
+const baseURL = 'heb ik nog niet'
+
+app.get('/', async function (request, response) {
+
+    response.render('home.liquid')
+})
