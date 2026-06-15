729fa0197efed844c465920f38b8c441b6f23666
Kayleighhhhh <kayleigh.vdham04@gmail.com>
2026-06-15 10:38:40 +0200

    404 page #7

--- server.js
+++ server.js
@@ -129,6 +129,11 @@ app.post('/:id', async function (request, response) {
     }
 })
 
+app.use((req, res, next) => {
+
+  res.status(404).render('error.liquid')
+})
+
 
 // Stel het poortnummer in waar Express op moet gaan luisteren
 // Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
